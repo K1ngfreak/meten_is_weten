f9fbdb8a4f9ba2e8a8fb0a3b14467358765198e4
Kingfreak <noahzonneveld@hotmail.com>
2021-09-15 16:10:00 +0200

    Min_en_Max

--- input_en_if-statement.py
+++ input_en_if-statement.py
@@ -3,9 +3,17 @@ B = input('Getal B: ')
 
 if A > B:
     Max = A
+    Min = B
     print('A is het grootste getal: ' + Max)
+    print('Maximum: ' + Max)
+    print('Minimum: ' + Min)
 elif A < B:
     Min = A
+    Max = B
     print('B is het kleinste getal: ' + Min)
-else: A == B
-print('A en B zijn even groot')
+    print('Maximum: ' + Max)
+    print('Minimum: ' + Min)
+else:
+    print('A en B zijn even groot')
+    print('Maximum: ' + A)
+    print('Minimum: ' + B)
